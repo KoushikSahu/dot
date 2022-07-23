69658509ab7ef8f071b14215e4f945e9f76972e0
Koushik Sahu <koushiksahu68@gmail.com>
2022-07-23 23:48:56 +0530

    add lua linux nvim configs, move old configs to legacy

---
+++ linux/.config/nvim/lua/keybindings/telescope.lua
@@ -0,0 +1,3 @@
+map("n", "<C-P>", "<cmd>lua require('telescope.builtin').find_files()<cr>")
+map("n", "<C-F>", "<cmd>lua require('telescope.builtin').live_grep()<cr>")
+
---
+++ linux/.config/nvim/lua/settings/init.lua
@@ -0,0 +1,20 @@
+local set = vim.opt
+
+-- core editor options
+set.clipboard = "unnamedplus"
+set.mouse = "a"
+set.hidden = true
+set.backspace = "indent,eol,start"
+
+-- appearance
+set.relativenumber = true
+set.cursorline = true
+set.ruler = true
+set.termguicolors = true
+
+-- indentations
+set.tabstop = 2
+set.shiftwidth = 2
+set.softtabstop = 2
+set.incsearch = true
+
